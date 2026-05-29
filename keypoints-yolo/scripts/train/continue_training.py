ea66a7538e9dcb7656905a9b1ac15ff4c4779108
Jakub Schwenkbeck <jakub@schwenkbeck.de>
2026-05-29 16:47:16 +0200

    Init module branch

--- keypoints-yolo/scripts/train/continue_training.py
+++
@@ -1,7 +0,0 @@
-# continue training from a checkpoint
-from ultralytics import YOLO
-
-checkpoint_path = ".../runs/pose/uncropped/quadruped/weights/last.pt"
-
-model = YOLO(checkpoint_path)
-model.train(resume=True)
